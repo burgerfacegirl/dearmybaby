40a73a357e76b8ee8dd12cf25651dec95b859adb
안승태 <sng_a@naver.com>
2023-01-26 15:04:05 +0900

    chore : project 기본세팅

---
+++ backend/src/test/java/com/ssafy/dmb/DmbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.dmb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
