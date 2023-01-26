11cc3be1e5821912aabf64dcf336a319ae2c9ad1
Junha9 <ghno20@gmail.com>
2023-01-26 16:24:21 +0900

    feat : create user entity

---
+++ backend/src/main/java/com/ssafy/dmb/domain/User.java
@@ -0,0 +1,40 @@
+package com.ssafy.dmb.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+@Entity
+public class User {
+
+    @Id @GeneratedValue
+    @Column(name = "user_no")
+    private Long no;
+
+    private String userName;
+
+    private String userEmail;
+
+    private String userPassword;
+
+    @Enumerated(EnumType.ORDINAL)
+    private UserValid uservalid; // 1 - valid, 2 - withdrawn
+
+    private String userId;
+
+    private String userImg;
+
+    @OneToMany(mappedBy = "user")
+    private List<GroupUser> groupUser = new ArrayList<>();
+
+
+
+
+
+
+
+}
