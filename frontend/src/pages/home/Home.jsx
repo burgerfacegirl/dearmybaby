613896d5fd800e40d939885866e7ff911256ea14
hojung <hoguangel@gmail.com>
2023-02-01 17:28:34 +0900

    feat : check dday

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,22 +1,41 @@
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Home.css';
 import Place from './Place';
+import Record from '../record';
 
 export default function Home() {
+  const [isStarted, setStarted] = useState(false);
+  const [isToday, checkDate] = useState(false);
   const navigate = useNavigate();
+  const travelDates = JSON.parse(localStorage.getItem('travelDates'));
+
+  console.log(localStorage.getItem('isToday'));
+  // D-day고 여행 시작 버튼 눌렀으면 redirect 시킴
+  // useEffect(() => {
+  //   JSON.parse(localStorage.getItem('isToday')) ? navigate(`/record`) : <></>;
+  // }, []);
+
+  // D-day - 두 날짜가 같은 날짜인지 확인하는 함수
+  const isSameDate = (date1, date2) => {
+    return (
+      date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate()
+    );
+  };
+
+  useEffect(() => {
+    if (travelDates != null && isSameDate(new Date(), new Date(travelDates[0].startDate))) {
+      checkDate(true);
+    }
+  }, []);
+
+  // console.log(isSameDate(new Date(), new Date(travelDates[0].startDate)));
   return (
     <div>
       <div className="userplan">
         <h3>계획 짜기</h3>
-
-        <ul className="" style={{ display: 'none' }}>
-          {/* <li onClick={}> 내 프로필</li> */}
-          <li> 내그룹</li>
-          <li> 장소 바구니</li>
-          <li> 내 여행</li>
-          <li> 공지사항</li>
-          <li> 고객 센터</li>
-        </ul>
         <button
           onClick={() => {
             navigate('/plan');
@@ -25,12 +44,26 @@ export default function Home() {
           여행 계획 추가하기
         </button>
       </div>
+      <hr />
 
       <h3>어린이와 겨울에 가기 좋은 여행지</h3>
       <Place />
 
       <h3>N세 어린이를 위한 추천 여행지</h3>
       <Place />
+
+      {isToday ? (
+        <button
+          onClick={() => {
+            setStarted(true);
+            localStorage.setItem('isToday', JSON.stringify(isStarted));
+          }}
+        >
+          여행 시작
+        </button>
+      ) : (
+        <Record />
+      )}
     </div>
   );
 }
--- frontend/src/pages/plan/SelectDate.jsx
+++ frontend/src/pages/plan/SelectDate.jsx
@@ -1,31 +1,40 @@
 import { DateRange } from 'react-date-range';
 import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
-
+// import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 const SelectDate = () => {
   const [state, setState] = useState([
     {
       startDate: new Date(),
-      endDate: null,
+      endDate: new Date(),
       key: 'selection',
     },
   ]);
 
+  // D-day - 이용자가 설정한 출발일과 오늘이 같은 날짜인지 확인
+  const isSameDate = (date1, date2) => {
+    return (
+      date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate()
+    );
+  };
+
   function saveTravelDates(e) {
     e.preventDefault();
-    // console.log('Start Date:', state[0].startDate);
-    // console.log('End Date : ', state[0].endDate);
     localStorage.setItem('travelDates', JSON.stringify(state));
   }
 
+  const travelDates = JSON.parse(localStorage.getItem('travelDates'));
+  // const navigate = useNavigate();
+
   function checkTravelDates(e) {
     e.preventDefault();
-    // console.log(new Date());
-    // if (new Date() === localStorage.getItem('travelDates')) {
-    //   console.log(localStorage.getItem('travelDates'));
-    // }
+    if (travelDates != null && isSameDate(new Date(), new Date(travelDates[0].startDate))) {
+      console.log('오늘은 여행날');
+    }
   }
 
   return (
