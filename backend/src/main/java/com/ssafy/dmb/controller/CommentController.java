85b4773a740cc55df3a4bac8f93a19be56bb301d
류태규 <estar1996@naver.com>
2023-02-02 14:31:12 +0900

    feat : comment controller 추가

---
+++ backend/src/main/java/com/ssafy/dmb/controller/CommentController.java
@@ -0,0 +1,35 @@
+package com.ssafy.dmb.controller;
+
+import com.ssafy.dmb.dto.CommentDto;
+import com.ssafy.dmb.dto.CommentResponseDto;
+import com.ssafy.dmb.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/comment")
+public class CommentController {
+    private final CommentService commentService;
+
+    @PostMapping()
+    public String saveComment(@RequestBody CommentDto commentDto) throws IOException {
+        System.out.println("요청옴");
+        commentService.saveComment(commentDto);
+        return "ok";
+    }
+
+    @GetMapping("/comments")
+    public List<CommentResponseDto> getCommentList(@RequestParam("recordId") Long recordId) throws IOException {
+        return commentService.getCommentList(recordId);
+    }
+
+    @DeleteMapping()
+    public void deleteComment(@RequestParam("commentId") Long commentId) throws IOException {
+        commentService.deleteComment(commentId);
+    }
+}
