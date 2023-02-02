a1ce3d22d7dd14fd81ade1ded4a43dfca38b4422
DaeheeKim <daehee.dev@gmail.com>
2023-02-02 10:42:54 +0900

    feat : modify HeadBar component

--- frontend/src/commons/components/HeadBar.jsx
+++ frontend/src/commons/components/HeadBar.jsx
@@ -1,26 +1,94 @@
-import LinkManager from '@/commons/components/LinkManager';
-import path from '@/config/path';
-import { useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const HeadBar = () => {
-  const HeadBarStyle = {
-    display: 'flex',
-    textAlign: 'center',
-    justifyContent: 'space-between',
-  };
-  const SideButton = {};
-  const navigate = useNavigate();
+import AppBar from '@mui/material/AppBar';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemButton from '@mui/material/ListItemButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+const links = [
+  { name: 'Home', path: '/' },
+  { name: 'Plan', path: 'plan' },
+  { name: 'Record', path: 'record' },
+  { name: 'Album', path: 'album' },
+];
+
+export default function HeadBar() {
+  const [open, setOpen] = useState(false);
+
+  function toggleDrawer(event, open) {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setOpen(open);
+  }
+
   return (
-    <div style={HeadBarStyle}>
-      <div>
-        <button onClick={() => navigate(-1)}>뒤 </button>
-        <LinkManager />
-      </div>
-      <div>
-        {/* <FontAwesomeIcon icon="fa-solid fa-bars" /> */}
-        <Link to={path.sidebar}><button style={SideButton}>=</button></Link>
-      </div>
-    </div>
+    <AppBar color="inherit" position="sticky">
+      <Toolbar>
+        <IconButton
+          component={Link}
+          to=".."
+          relative="path"
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <ArrowBackIcon />
+        </IconButton>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+            DearMyBaby
+          </Link>
+        </Typography>
+        <IconButton
+          size="large"
+          edge="end"
+          color="inherit"
+          aria-label="menu"
+          sx={{ ml: 2 }}
+          onClick={(event) => toggleDrawer(event, true)}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Drawer anchor="right" open={open} onClose={(event) => toggleDrawer(event, false)}>
+          <Box
+            sx={{ width: '50vw' }}
+            role="presentation"
+            onClick={(event) => toggleDrawer(event, false)}
+            onKeyDown={(event) => toggleDrawer(event, false)}
+          >
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton component={Link} to="user">
+                  <Avatar sx={{ width: 32, height: 32, mr: 2 }}>M</Avatar>
+                  <ListItemText primary="MyAccount"></ListItemText>
+                </ListItemButton>
+              </ListItem>
+              <Divider></Divider>
+              {links.map((link) => (
+                <ListItem key={link.path} disablePadding>
+                  <ListItemButton component={Link} to={link.path}>
+                    <ListItemText primary={link.name}></ListItemText>
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Drawer>
+      </Toolbar>
+    </AppBar>
   );
-};
-export default HeadBar;
+}
