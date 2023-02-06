56523b8a451aeecb3087a86e76247295dd0bc49d
안승태 <sng_a@naver.com>
2023-02-06 22:24:29 +0900

    chore : jwt setting

--- backend/src/main/java/com/ssafy/dmb/domain/User.java
+++ backend/src/main/java/com/ssafy/dmb/domain/User.java
@@ -2,15 +2,20 @@ package com.ssafy.dmb.domain;
 
 import com.ssafy.dmb.domain.record.Comment;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter @Setter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User {
+public class User implements UserDetails {
 
     @Id @GeneratedValue
     @Column(name = "user_no")
@@ -37,12 +42,53 @@ public class User {
     private List<Comment> comments = new ArrayList<>();
 
     @Builder
-    public User(String userName, String userEmail, String userPassword, String userId, String userImg) {
+    public User(String userName, String userEmail, String userPassword, String userId, String userImg,List<String> roles) {
         this.userName = userName;
         this.userEmail = userEmail;
         this.userPassword = userPassword;
         this.userId = userId;
         this.userImg = userImg;
+        this.roles = roles;
     }
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return userPassword;
+    }
+
+    @Override
+    public String getUsername() {
+        return userId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- backend/src/main/java/com/ssafy/dmb/service/UserService.java
+++ backend/src/main/java/com/ssafy/dmb/service/UserService.java
@@ -1,13 +1,18 @@
 package com.ssafy.dmb.service;
 
 import com.ssafy.dmb.domain.User;
+import com.ssafy.dmb.dto.jwt.TokenInfo;
 import com.ssafy.dmb.dto.user.UserDto;
 import com.ssafy.dmb.dto.user.UserResponseDto;
+import com.ssafy.dmb.jwt.JwtTokenProvider;
 import com.ssafy.dmb.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +25,9 @@ public class UserService {
     private final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
 
     private final UserRepository userRepository;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final JwtTokenProvider jwtTokenProvider;
+
 
     public UserResponseDto getUserDetail(String userId) {
         User userDetail = userRepository.findByUserId(userId);
@@ -74,5 +82,21 @@ public class UserService {
 
         if(user == null) return true;
         else return false;
+    }
+
+    @Transactional
+    public TokenInfo login(String memberId, String password) {
+        // 1. Login ID/PW 를 기반으로 Authentication 객체 생성
+        // 이때 authentication 는 인증 여부를 확인하는 authenticated 값이 false
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(memberId, password);
+
+        // 2. 실제 검증 (사용자 비밀번호 체크)이 이루어지는 부분
+        // authenticate 매서드가 실행될 때 CustomUserDetailsService 에서 만든 loadUserByUsername 메서드가 실행
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        // 3. 인증 정보를 기반으로 JWT 토큰 생성
+        TokenInfo tokenInfo = jwtTokenProvider.generateToken(authentication);
+
+        return tokenInfo;
     }
 }
