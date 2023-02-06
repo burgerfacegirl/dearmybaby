c58dd89e5f80242835a1248369fc490fc7cc739c
DaeheeKim <daehee.dev@gmail.com>
2023-02-06 14:33:45 +0900

    feat : use environment variable at axios

--- frontend/src/commons/api/http.jsx
+++ frontend/src/commons/api/http.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function getApiInstance() {
   const instance = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
       'Content-Type': 'application/json;charset=utf-8',
     },
