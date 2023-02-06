6de95a3344deee3c60b3ee44a801406efe0acf35
안승태 <sng_a@naver.com>
2023-02-06 10:59:51 +0900

    feat : add checklist entity

---
+++ backend/src/main/java/com/ssafy/dmb/domain/plan/Checklist.java
@@ -0,0 +1,29 @@
+package com.ssafy.dmb.domain.plan;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Checklist {
+
+    @Id @GeneratedValue
+    @Column(name = "checklist_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "plan_id")
+    private Plan plan;
+
+    private String item;
+
+    @Builder
+    public Checklist(Long id, Plan plan, String item) {
+        this.id = id;
+        this.plan = plan;
+        this.item = item;
+    }
+}
