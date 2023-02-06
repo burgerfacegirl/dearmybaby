bd8107061a91f547de7f28c80e21dfa55285e5d3
안승태 <sng_a@naver.com>
2023-02-06 11:01:30 +0900

    feat : add checklist service

---
+++ backend/src/main/java/com/ssafy/dmb/service/ChecklistService.java
@@ -0,0 +1,56 @@
+package com.ssafy.dmb.service;
+
+
+import com.ssafy.dmb.domain.plan.Checklist;
+import com.ssafy.dmb.domain.plan.Plan;
+import com.ssafy.dmb.dto.Plan.ChecklistResponseDto;
+import com.ssafy.dmb.dto.Plan.ChecklistDto;
+import com.ssafy.dmb.repository.ChecklistRepository;
+import com.ssafy.dmb.repository.PlanRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+@Service
+public class ChecklistService {
+    private final ChecklistRepository checklistRepository;
+    private final PlanRepository planRepository;
+
+    public ChecklistResponseDto saveChecklist(ChecklistDto checklistDto) {
+        Long planId = checklistDto.getPlanId();
+
+        Plan plan = planRepository.findById(planId).
+                orElseThrow(() -> new NoSuchElementException());
+
+        Checklist checklist = Checklist.builder()
+                .plan(plan)
+                .item(checklistDto.getItem())
+                .build();
+        checklistRepository.save(checklist);
+        return null;
+    }
+
+    public List<ChecklistResponseDto> getChecklist(Long planId) {
+        List<Checklist> checklistList = checklistRepository.findAllByPlanId(planId);
+        List<ChecklistResponseDto> ItemList = checklistList.stream()
+                .map(t -> new ChecklistResponseDto(t))
+                .collect(Collectors.toList());
+
+        return ItemList;
+
+    }
+
+    public void deleteItem(Long itemId) {
+        checklistRepository.deleteById(itemId);
+    }
+
+
+}
