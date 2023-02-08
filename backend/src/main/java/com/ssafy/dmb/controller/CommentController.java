ba7ed16721a98b22815ef0e2b7d8606a5f8e58e3
안승태 <sng_a@naver.com>
2023-02-08 21:56:52 +0900

    style : commentcontroller

--- backend/src/main/java/com/ssafy/dmb/controller/CommentController.java
+++ backend/src/main/java/com/ssafy/dmb/controller/CommentController.java
@@ -2,7 +2,6 @@ package com.ssafy.dmb.controller;
 
 import com.ssafy.dmb.dto.comment.CommentDto;
 import com.ssafy.dmb.dto.comment.CommentResponseDto;
-import com.ssafy.dmb.repository.RecordRepository;
 import com.ssafy.dmb.service.CommentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -13,34 +12,41 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.List;
 
-
 @RequiredArgsConstructor
 @RestController
 @Tag(name = "Comment", description = "comment API 입니다.")
 @RequestMapping("/api/comment")
 public class CommentController {
+
     private final CommentService commentService;
-    private final RecordRepository recordRepository;
 
     @Transactional
     @Operation(summary = "댓글 저장", description = "댓글을 저장한다.")
     @PostMapping()
     public List<CommentResponseDto> saveComment(@RequestBody CommentDto commentDto) throws IOException {
+
         commentService.saveComment(commentDto);
         return commentService.getCommentList(commentDto.getRecordId());
+
     }
 
     @Operation(summary = "댓글 리스트 조회", description = "<strong> recordId </strong>를 통해 댓글 리스트를 전체 조회한다.")
     @GetMapping("/comments")
     public List<CommentResponseDto> getCommentList(@RequestParam("recordId") Long recordId) throws IOException {
+
         return commentService.getCommentList(recordId);
+
     }
 
     @Transactional
     @Operation(summary = "댓글 삭제", description = "<strong> commentId </strong>를 통해 댓글을 삭제한다.")
     @DeleteMapping()
-    public List<CommentResponseDto> deleteComment(@RequestParam("commentId") Long commentId, @RequestParam("recordId") Long recordId) throws IOException {
+    public List<CommentResponseDto> deleteComment(@RequestParam("commentId") Long commentId
+            , @RequestParam("recordId") Long recordId) throws IOException {
+
         commentService.deleteComment(commentId);
         return commentService.getCommentList(recordId);
+
     }
+
 }
