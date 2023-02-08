612787b5077712b2c76695cef91b50bc62e5b06b
안승태 <sng_a@naver.com>
2023-02-08 21:51:34 +0900

    fix : commentdelete api

--- backend/src/main/java/com/ssafy/dmb/controller/CommentController.java
+++ backend/src/main/java/com/ssafy/dmb/controller/CommentController.java
@@ -2,11 +2,12 @@ package com.ssafy.dmb.controller;
 
 import com.ssafy.dmb.dto.comment.CommentDto;
 import com.ssafy.dmb.dto.comment.CommentResponseDto;
+import com.ssafy.dmb.repository.RecordRepository;
 import com.ssafy.dmb.service.CommentService;
-import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -19,6 +20,7 @@ import java.util.List;
 @RequestMapping("/api/comment")
 public class CommentController {
     private final CommentService commentService;
+    private final RecordRepository recordRepository;
 
     @Operation(summary = "댓글 저장", description = "댓글을 저장한다.")
     @PostMapping()
@@ -34,9 +36,11 @@ public class CommentController {
         return commentService.getCommentList(recordId);
     }
 
+    @Transactional
     @Operation(summary = "댓글 삭제", description = "<strong> commentId </strong>를 통해 댓글을 삭제한다.")
     @DeleteMapping()
-    public void deleteComment(@RequestParam("commentId") Long commentId) throws IOException {
+    public List<CommentResponseDto> deleteComment(@RequestParam("commentId") Long commentId, @RequestParam("recordId") Long recordId) throws IOException {
         commentService.deleteComment(commentId);
+        return commentService.getCommentList(recordId);
     }
 }
