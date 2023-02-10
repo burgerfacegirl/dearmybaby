1a7f9be270418b591f42ac030ad34e211ab80d0c
안승태 <sng_a@naver.com>
2023-02-10 14:24:40 +0900

    feat : add kidsinfo page

--- frontend/src/pages/user/KidsInformation.jsx
+++ frontend/src/pages/user/KidsInformation.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import './KidsInformation.css';
 import { useNavigate } from 'react-router-dom';
 import { height } from '@mui/system';
+import questionList from './KidData.json';
 
 const dummyChildFavor = [
   {
@@ -16,124 +17,144 @@ const dummyChildFavor = [
 ];
 const qNum = [...new Array(2)].map((_, i) => i + 1);
 export default function KidsInformation() {
-  const [food, setFood] = useState([]);
-  const [place, setPlace] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [placeList, setPlaceList] = useState([]);
+  const [foodList, setFoodList] = useState([]);
   const navigate = useNavigate();
-  let quizAnswer = [
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-  ];
-  const [pointer, setPointer] = useState(0);
-  const [answer, setAnswer] = useState([
-    true,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-  ]);
+  const question = questionList[index];
 
-  // 다음 문제로 가기
-  function toNext() {
-    quizAnswer[pointer] = false;
-    quizAnswer[pointer + 1] = true;
-    // console.log(quizAnswer);
-    setAnswer(quizAnswer);
-    setPointer(pointer + 1);
-    // console.log('pointer', pointer, 'quizANswer', quizAnswer);
-  }
-  // 뒤로가기
-  const goBack = () => {
-    quizAnswer[pointer] = false;
-    quizAnswer[pointer - 1] = true;
-    setAnswer(quizAnswer);
-    setPointer(pointer - 1);
-    console.log(pointer);
+  const handleYes = () => {
+    const categoryId = question.categoryId;
+    if (question.type == 'place') {
+      if (!placeList.includes(categoryId)) {
+        setPlaceList([...placeList, categoryId]);
+      }
+    } else if (question.type == 'food') {
+      if (!foodList.includes(categoryId)) {
+        setFoodList([...foodList, categoryId]);
+      }
+    }
+    setIndex(index + 1);
   };
+
+  const handleNo = () => {
+    setIndex(index + 1);
+  };
+
+  // const [food, setFood] = useState([]);
+  // const [place, setPlace] = useState([]);
+  // let quizAnswer = [
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  // ];
+  // const [pointer, setPointer] = useState(0);
+  // const [answer, setAnswer] = useState([
+  //   true,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  // ]);
+
+  // // 다음 문제로 가기
+  // function toNext() {
+  //   quizAnswer[pointer] = false;
+  //   quizAnswer[pointer + 1] = true;
+  //   // console.log(quizAnswer);
+  //   setAnswer(quizAnswer);
+  //   setPointer(pointer + 1);
+  //   // console.log('pointer', pointer, 'quizANswer', quizAnswer);
+  // }
+  // // 뒤로가기
+  // const goBack = () => {
+  //   quizAnswer[pointer] = false;
+  //   quizAnswer[pointer - 1] = true;
+  //   setAnswer(quizAnswer);
+  //   setPointer(pointer - 1);
+  //   console.log(pointer);
+  // };
   // https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBEIwupfrAKsMRJP3yTVIdF-fq2e3N0D5YZA&usqp=CAU
   return (
-    <div className="question">
-      <button style={{ position: 'absolute' }} onClick={() => console.log(place, answer)}>
-        Panic Button
-      </button>
-      <div className={answer[0] ? 'show' : 'noshow'} style={{ position: 'absolute', height: '100vh', width: '100vw' }}>
-        <div style={{ marginBlock: '27%', padding: '15%' }}>
-          <h2 style={{ fontSize: '2.4rem' }}> 바다 </h2>
-        </div>
+    <>
+      <div className="question">
+        <button style={{ position: 'absolute' }} onClick={() => console.log(placeList, foodList)}>
+          Panic Button
+        </button>
+        {index < questionList.length ? (
+          <div>
+            <div style={{ position: 'absolute' }}>
+              <div style={{ marginBlock: '27%', padding: '15%' }}>
+                <h2 style={{ fontSize: '2.4rem' }}>{question.categoryName}</h2>
+              </div>
 
-        <div className="answer-div" style={{ display: 'flex', width: '70%', justifyContent: 'space-between' }}>
-          <button
-            className="kidinfo-button"
-            onClick={() => {
-              if (!place.includes('sea')) {
-                setPlace([...place, 'sea']);
-              }
-              toNext();
-            }}
-          >
-            좋아한다
-          </button>
-          {/* // 다음 질문로 넘어가는 함수 */}
-          <button className="kidinfo-button" onClick={toNext}>
-            싫어한다
-          </button>
-        </div>
-        <div className="go-back">
-          <button
-            style={{ backgroundColor: 'white', color: 'blue', border: 'blue' }}
-            onClick={() => {
-              navigate(-1);
-            }}
-          >
-            뒤로
-          </button>
-        </div>
+              <div className="answer-div" style={{ display: 'flex', width: '70%', justifyContent: 'space-between' }}>
+                <button className="kidinfo-button" onClick={handleYes}>
+                  좋아한다
+                </button>
+                {/* // 다음 질문로 넘어가는 함수 */}
+                <button className="kidinfo-button" onClick={handleNo}>
+                  싫어한다
+                </button>
+              </div>
+              <div className="go-back">
+                <button
+                  style={{ backgroundColor: 'white', color: 'blue', border: 'blue' }}
+                  onClick={() => {
+                    navigate(-1);
+                  }}
+                >
+                  뒤로
+                </button>
+              </div>
+            </div>
+            <img src={question.imgUrl} style={{ height: '100%', width: '100%' }} alt="questionError"></img>
+          </div>
+        ) : (
+          <div>질문이 다 끝났다</div>
+        )}
+
+        {/* className={answer[0] ? 'show' : 'noshow'} */}
+        {/* ========================= */}
       </div>
-      <img
-        src="https://i.pinimg.com/originals/a1/2a/60/a12a603f00c65d286b936a323711fc5d.gif"
-        style={{ height: '100%', width: '100%' }}
-        alt="questionError"
-      ></img>
-      {/* className={answer[0] ? 'show' : 'noshow'} */}
-      {/* ========================= */}
-    </div>
+    </>
   );
 }
