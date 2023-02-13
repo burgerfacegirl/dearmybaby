0c334d2b632db7713875ce59cba6abfcfadb23d5
estar1996 <estar19961996@gmail.com>
2023-02-13 21:40:34 +0900

    style : mypage css add

--- frontend/src/pages/user/UserInfo.jsx
+++ frontend/src/pages/user/UserInfo.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
-
+import './Userinfo.css';
 import { useMember, useMemberReload } from '@/commons/MemberContext';
 
 /** API 설명용 */
 import { useState } from 'react';
 import { useMemberAuth } from '@/commons/MemberContext';
 import { apiGetMember } from '@/commons/api/member';
+import { green } from '@mui/material/colors';
 
 export default function UserInfo() {
   const member = useMember();
@@ -16,46 +17,95 @@ export default function UserInfo() {
   const [testMember, setTestMember] = useState(null);
 
   return (
-    <div>
-      <h1>회원정보</h1>
-      <div>
-        <ul>
-          <li>
-            <Link to="login">로그인하기</Link>
-          </li>
-          <li>
-            <Link to="sign-up">회원가입하기</Link>
-          </li>
-        </ul>
-      </div>
-      <hr></hr>
-      <button onClick={memberReload}>memberReload1</button>
-      {!!member && (
+    <div style={{backgroundColor:'rgba(255, 255, 255,0.922'}}>
+      <div style={{}}>
+        <h2 style={{margin:'5px'}}>회원정보</h2>
         <div>
           <ul>
-            <li>{member.memberName}</li>
-            <li>{member.memberEmail}</li>
-            <li>{member.memberId}</li>
-            <li>{member.memberImg}</li>
+            <li>
+              <Link to="login">로그인하기</Link>
+            </li>
+            <li>
+              <Link to="sign-up">회원가입하기</Link>
+            </li>
           </ul>
+        </div>
+      </div>
+      {/* <hr></hr> */}
+    
+      <button style={{backgroundColor:'white',
+      borderRadius:'5px',
+      color:'rgb(1 116 163)',
+      border:'2px solid rgb(1 116 163)',
+      margin:'5px',
+      fontWeight:'800',
+      marginLeft: '25px',
+      marginRight: '25px'
+      }}
+      
+      onClick={memberReload}>memberReload1</button>
+      {!!member && (
+        <div >
+
+          <div style={{display:'flex', justifyContent: 'center'}}>
+            <img
+              src="/assets/baby.jpg"
+              style={{ height: '110px', width: '110px', borderRadius: '50%', boxShadow: '0px 2px 2px'}}
+              alt="img"
+            />
+          </div>
+          <div style={{padding:'20px', borderRadius:'10px', backgroundColor:'rgb(1,1,1,0.03)'}}>
+            <ul style={{marginRight:'30px'}}>
+              <li style={{color:'grey',padding:'5px'}}>이름</li>
+              <li style={{fontSize:'20px'}}>{member.memberName}</li>
+              <hr style={{backgroundColor:'#b7449c', height:'3px'}}></hr>
+              <li style={{color:'grey',padding:'5px',paddingTop:'10px'}}>이메일</li>
+              <li style={{fontSize:'20px'}}>{member.memberEmail}</li>
+              <hr style={{backgroundColor:'#b7449c', height:'3px'}}></hr>
+              <li style={{color:'grey',padding:'5px',paddingTop:'10px'}}>닉네임</li>
+              <li style={{fontSize:'20px'}}>{member.memberId}</li>
+              <hr style={{backgroundColor:'#b7449c', height:'3px'}}></hr>
+            </ul>
+          </div>
+          <li>{member.memberImg}</li>
           {/* <p>{JSON.stringify(member)}</p> */}
         </div>
       )}
-      <hr></hr>
-      <button onClick={() => auth((token) => apiGetMember(token)).then(({ data }) => setTestMember(data))}>
-        memberReload2
-      </button>
-      {!!testMember && (
-        <div>
-          <ul>
-            <li>{testMember.memberName}</li>
-            <li>{testMember.memberEmail}</li>
-            <li>{testMember.memberId}</li>
-            <li>{testMember.memberImg}</li>
-          </ul>
-          {/* <p>{JSON.stringify(testMember)}</p> */}
-        </div>
-      )}
+
+
     </div>
   );
 }
+
+
+
+
+
+
+
+
+// <button style={{backgroundColor:'rgb(255,255,255)',
+// borderRadius:'5px',
+// color:'rgb(1 116 163)',
+// border:'2px solid rgb(1 116 163)',
+// margin:'5px',
+// fontWeight:'800',
+// marginLeft:'25px',
+// marginRight: '25px'
+
+// }}
+//  onClick={() => auth((token) => apiGetMember(token)).then(({ data }) => setTestMember(data))}>
+//   memberReload2
+// </button>
+// {!!testMember && (
+//   <div>
+//     <ul>
+//       <li>이름 : {testMember.memberName}</li>
+//       <li>이메일 : {testMember.memberEmail}</li>
+//       <li>닉네임 : {testMember.memberId}</li>
+//       <li>{testMember.memberImg}</li>
+//     </ul>
+
+//   </div>
+// )}
+          {/* <p>{JSON.stringify(testMember)}</p> */}
